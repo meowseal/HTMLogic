046e0fd56c23fea0bb454985ebab3f0153fe034c
meowseal <hotdogtoiletcleaner@gmail.com>
2025-10-16 12:02:37 -0500

    Create refrences.ts

---
+++ main/refrences.ts
@@ -0,0 +1,15 @@
+// Basic Functions
+function print(input:any) {
+    console.log(input)
+}
+
+// Object Shapes
+interface  BaseLogic {
+    A: number;
+    B: number;
+    Out: number;
+    Size: {
+
+    }
+}
+
